29389dc1907089cd921c723261f5363358ba6cc2
ChinhLP <lpchinh25@gmail.com>
2026-02-15 21:48:51 +0700

    first commit

---
+++ common/emoji-picker/js/index.js
@@ -0,0 +1,1 @@
+import Picker from"./picker.js";import Database from"./database.js";export{Picker,Database};
---
+++ wedding-data.js
@@ -0,0 +1,4 @@
+const weddingData = {
+    groom: {fullName: "Phạm Đình Long", shortName: "Đình Long", birthday: "24/01/1998"},
+    bride: {fullName: "Nguyễn Thanh hiền", shortName: "Thanh hiền", birthday: "28/12/1998"}
+};
